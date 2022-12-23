4de40f1152cfc1e94fa44a9f09e644d31daa8707
Pritish Bhawal <pritishyo10@gmail.com>
2022-12-23 16:18:31 +0530

    2nd qs

---
+++ recursion1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int X=100;
+    int N=3;
+    int limit = pow(X,1.0/N);
+    cout<<limit;
+    return 0;
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,3 @@
+ if(stoi(n)<10){
+        return stoi(n);
+    }
